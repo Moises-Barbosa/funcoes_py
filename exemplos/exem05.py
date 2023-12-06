dffe4053a1e8b87f8c0d62526df4f45620f06499
Moises-Barbosa <moises-_-barbosa@outlook.com>
2023-12-06 16:11:28 -0300

    Incluindo outra forma de fazer

--- exemplos/exem05.py
+++ exemplos/exem05.py
@@ -7,3 +7,12 @@ def media(a):
   return soma(a)/len(a)
 list = [4, 6, 5, 10, -5]
 print(media(list))
+
+#outra forma de fazer
+
+def media(a):
+  soma = 0
+  for i in a:
+    soma += i
+  return soma/len(a)
+print(media(list)) 
